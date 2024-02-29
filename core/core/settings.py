e1c84f56737ebb2afd04e65a3dd33f1ed0928ab7
Lobov Sergey <loboff09@gmail.com>
2024-02-29 21:48:02 +0300

    base

--- core/core/settings.py
+++ core/core/settings.py
@@ -127,3 +127,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "FILES")
 MEDIA_URL = "/FILES/"
+
+
+REDIS_HOST = "0.0.0.0"
+REDIS_PORT = "6379"
+CELERY_BROKER_URL = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
+CELERY_RESULT_BACKEND = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
--- core/core/urls.py
+++ core/core/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path("upload/", FileUploadView.as_view()),
     path("files/", FileListView.as_view()),
 ]
-static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
